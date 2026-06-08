b0be7e4e2ffa7b214c0ef90ffb7ebc2a71230de2
LeoVera-dev <l.vera31415@gmail.com>
2026-06-08 00:22:22 -0500

    updated to lazyvim because packer is unc also added better undo and checking out errors in code

--- init.lua
+++ init.lua
@@ -1,317 +1,309 @@
--- Bootstrap packer.nvim
---
-local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
-end
+-- Settings
+vim.opt.number         = true
+vim.opt.relativenumber = true
+vim.opt.signcolumn     = "yes"
+vim.opt.wrap           = false
+vim.opt.tabstop        = 2
+vim.opt.shiftwidth     = 2
+vim.opt.expandtab      = true
+vim.opt.smartindent    = true
+vim.opt.swapfile       = false
+vim.opt.undofile       = true
+vim.opt.termguicolors  = true
+vim.opt.updatetime     = 250
+vim.opt.timeoutlen     = 300
+vim.opt.completeopt    = { "menu", "menuone", "noselect" }
+vim.opt.winborder      = "rounded"
+vim.g.mapleader        = " "
 
-local packer_bootstrap = ensure_packer()
+-- Keymaps
+local map = vim.keymap.set
 
--- Suppress vim.tbl_islist deprecation warnings (temporary fix for plugin compatibility)
-vim.deprecate = function() end
+-- General
+map("n", "<leader>o", ":update<CR>",           { desc = "Save" })
+map("n", "<leader>w", ":write<CR>",            { desc = "Write" })
+map("n", "<leader>q", ":quit<CR>",             { desc = "Quit" })
 
--- Basic Settings
-vim.wo.number = true
-vim.wo.relativenumber = true
-vim.o.signcolumn = "yes"
-vim.opt.wrap = false
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
-vim.opt.swapfile = false
-vim.opt.smartindent = true
-vim.g.mapleader = " "
-vim.o.winborder = "rounded"
-vim.opt.updatetime = 250
-vim.opt.timeoutlen = 300
-vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
+-- Plugins
+map("n", "<leader>O", ":Oil<CR>",              { desc = "Open Oil" })
+map("n", "<leader>M", ":Mason<CR>",            { desc = "Open Mason" })
+map("n", "<leader>u", vim.cmd.UndotreeToggle,  { desc = "Toggle Undotree" })
 
--- Key Mappings
-vim.keymap.set('n', '<leader>o', ':update<CR>:source<CR>')
-vim.keymap.set('n', '<leader>w', ':write<CR>')
-vim.keymap.set('n', '<leader>q', ':quit<CR>')
-vim.keymap.set('n', '<leader>O', ":Oil<CR>")
-vim.keymap.set('n', '<leader>M', ":Mason<CR>")
-vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
-vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
-
--- LSP Attach Keymaps
-vim.api.nvim_create_autocmd('LspAttach', {
+-- LSP (buffer-local, set on LspAttach)
+vim.api.nvim_create_autocmd("LspAttach", {
   callback = function(ev)
     local opts = { buffer = ev.buf }
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
-    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+    local function lmap(key, fn, desc)
+      map("n", key, fn, vim.tbl_extend("force", opts, { desc = desc }))
+    end
+
+    lmap("gd",          vim.lsp.buf.definition,                      "Go to definition")
+    lmap("K",           vim.lsp.buf.hover,                           "Hover docs")
+    lmap("gr",          vim.lsp.buf.references,                      "References")
+    lmap("<leader>rn",  vim.lsp.buf.rename,                          "Rename symbol")
+    lmap("<leader>ca",  vim.lsp.buf.code_action,                     "Code action")
+    lmap("<leader>lf",  function()
+      vim.lsp.buf.format({ async = false, timeout_ms = 2000 })
+    end, "Format buffer")
+
+    lmap("[d",          vim.diagnostic.goto_prev,                    "Prev diagnostic")
+    lmap("]d",          vim.diagnostic.goto_next,                    "Next diagnostic")
+    lmap("<leader>e",   vim.diagnostic.open_float,                   "Show diagnostic")
+    lmap("<leader>dl",  vim.diagnostic.setloclist,                   "Diagnostic list")
   end,
 })
 
--- Plugin Setup
-require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
-  
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.uv.fs_stat(lazypath) then
+  vim.fn.system({
+    "git", "clone", "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Plugins
+require("lazy").setup({
+
   -- Colorscheme
-  use 'edeneast/nightfox.nvim'
-  
-  -- File Explorer
-  use {
-    'stevearc/oil.nvim',
+  {
+    "edeneast/nightfox.nvim",
+    lazy     = false,
+    priority = 1000,
+    config = function()
+      vim.o.background = "dark"
+      vim.cmd.colorscheme("carbonfox")
+      vim.cmd(":hi statusline guibg=NONE")
+    end,
+  },
+
+  -- File explorer
+  {
+    "stevearc/oil.nvim",
+    lazy = false,
     config = function()
       require("oil").setup({
         default_file_explorer = true,
-        columns = {
-          "permissions",
-          "size",
-          "mtime",
-        },
-        view_options = {
-          show_hidden = true,
-        },
+        columns      = { "permissions", "size", "mtime" },
+        view_options = { show_hidden = true },
       })
     end,
-  }
-  -- Undo tree
-  use "mbbill/undotree"
-  -- Fuzzy Finder
-  use {
-    'nvim-telescope/telescope.nvim',
-    requires = { 'nvim-lua/plenary.nvim' },
+  },
+
+  -- Undo history visualizer
+  { "mbbill/undotree" },
+
+  -- Fuzzy finder
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
-      require('telescope').setup({
+      local actions = require("telescope.actions")
+      local builtin = require("telescope.builtin")
+      require("telescope").setup({
         defaults = {
           mappings = {
             i = {
-              ['<C-j>'] = require('telescope.actions').move_selection_next,
-              ['<C-k>'] = require('telescope.actions').move_selection_previous,
+              ["<C-j>"] = actions.move_selection_next,
+              ["<C-k>"] = actions.move_selection_previous,
             },
           },
         },
       })
-      
-      local builtin = require('telescope.builtin')
-      vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-      vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-      vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-      vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-      vim.keymap.set('n', '<leader>fr', builtin.lsp_references, {})
-      vim.keymap.set('n', '<leader>fs', builtin.lsp_document_symbols, {})
+      map("n", "<leader>ff", builtin.find_files,           { desc = "Find files" })
+      map("n", "<leader>fg", builtin.live_grep,            { desc = "Live grep" })
+      map("n", "<leader>fb", builtin.buffers,              { desc = "Buffers" })
+      map("n", "<leader>fh", builtin.help_tags,            { desc = "Help tags" })
+      map("n", "<leader>fr", builtin.lsp_references,       { desc = "LSP references" })
+      map("n", "<leader>fs", builtin.lsp_document_symbols, { desc = "LSP symbols" })
     end,
-  }
-  
-  -- Icons
-  use 'nvim-tree/nvim-web-devicons'
-  
-  -- Treesitter
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    run = function()
-      local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
-      ts_update()
-    end,
-  }
-  
-  -- LSP
-  use 'neovim/nvim-lspconfig'
-  use 'williamboman/mason.nvim'
-  use 'williamboman/mason-lspconfig.nvim'
-  use 'nvim-mini/mini.nvim'
+  },
 
+  -- Icons
+  { "nvim-tree/nvim-web-devicons" },
+
+  -- Treesitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = function()
+      require("nvim-treesitter.install").update({ with_sync = true })()
+    end,
+    config = function()
+      local ok, configs = pcall(require, "nvim-treesitter.configs")
+      if not ok then return end
+      configs.setup({
+        ensure_installed = {
+          "lua", "python", "c", "go", "rust",
+          "bash", "javascript", "typescript",
+          "html", "css", "json", "markdown",
+        },
+        sync_install  = false,
+        auto_install  = true,
+        highlight     = { enable = true, additional_vim_regex_highlighting = false },
+        indent        = { enable = true },
+      })
+    end,
+  },
+
+  -- LSP
+  { "neovim/nvim-lspconfig" },
+
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = { "williamboman/mason.nvim", "neovim/nvim-lspconfig" },
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "pyright", "clangd", "rust_analyzer", "bashls", "ts_ls" },
+        automatic_installation = true,
+      })
+    end,
+  },
+
+  {
+    "echasnovski/mini.icons",
+    version = false,
+    config = function()
+      require("mini.icons").setup()
+    end,
+  },
 
   -- Completion
-  use 'hrsh7th/nvim-cmp'
-  use 'hrsh7th/cmp-nvim-lsp'
-  use({
-    "L3MON4D3/LuaSnip",
-    tag = "v2.*",
-    run = "make install_jsregexp"
-  })
-  use 'saadparwaiz1/cmp_luasnip'
-
-  --Notes
-  use({
-    'MeanderingProgrammer/render-markdown.nvim',
-    after = { 'nvim-treesitter' },
-    requires = { 'nvim-mini/mini.nvim', opt = true },            -- if you use the mini.nvim suite
-    -- requires = { 'nvim-mini/mini.icons', opt = true },        -- if you use standalone mini plugins
-    -- requires = { 'nvim-tree/nvim-web-devicons', opt = true }, -- if you prefer nvim-web-devicons
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      { "L3MON4D3/LuaSnip", version = "v2.*", build = "make install_jsregexp" },
+      "saadparwaiz1/cmp_luasnip",
+    },
     config = function()
-        require('render-markdown').setup({})
+      local cmp     = require("cmp")
+      local luasnip = require("luasnip")
+      cmp.setup({
+        snippet = {
+          expand = function(args) luasnip.lsp_expand(args.body) end,
+        },
+        window = {
+          completion    = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-b>"]     = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"]     = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"]     = cmp.mapping.abort(),
+          ["<CR>"]      = cmp.mapping.confirm({ select = true }),
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+        }, {
+          { name = "buffer" },
+          { name = "path" },
+        }),
+      })
     end,
-})
-  
+  },
+
+  -- Markdown rendering
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" },
+    config = function()
+      require("render-markdown").setup({})
+    end,
+  },
+
+  -- Image display (Kitty / Ghostty protocol)
+  {
+    "3rd/image.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    config = function()
+      require("image").setup({
+        backend = "kitty",
+        integrations = {
+          markdown = {
+            enabled                     = true,
+            clear_in_insert_mode        = false,
+            download_remote_images      = true,
+            only_render_image_at_cursor = false,
+            filetypes                   = { "markdown", "vimwiki" },
+          },
+        },
+        max_height_window_percentage = 50,
+        kitty_method                 = "normal",
+      })
+    end,
+  },
+
   -- Dashboard
-  use {
-    'goolord/alpha-nvim',
+  {
+    "goolord/alpha-nvim",
+    lazy = false,
     config = function()
-      local alpha = require('alpha')
-      local dashboard = require('alpha.themes.dashboard')
-      
+      local alpha     = require("alpha")
+      local dashboard = require("alpha.themes.dashboard")
       dashboard.section.header.val = {
-      "███    ██ ███████  ██████  ██    ██ ██ ███    ███",
-      "████   ██ ██      ██    ██ ██    ██ ██ ████  ████",
-      "██ ██  ██ █████   ██    ██ ██    ██ ██ ██ ████ ██",
-      "██  ██ ██ ██      ██    ██  ██  ██  ██ ██  ██  ██",
-      "██   ████ ███████  ██████    ████   ██ ██      ██"
+        "███    ██ ███████  ██████  ██    ██ ██ ███    ███",
+        "████   ██ ██      ██    ██ ██    ██ ██ ████  ████",
+        "██ ██  ██ █████   ██    ██ ██    ██ ██ ██ ████ ██",
+        "██  ██ ██ ██      ██    ██  ██  ██  ██ ██  ██  ██",
+        "██   ████ ███████  ██████    ████   ██ ██      ██",
       }
-      
       dashboard.section.buttons.val = {
-        dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
-        dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
-        dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
-        dashboard.button("o", "  Open Oil", ":Oil <CR>"),
-        dashboard.button("g", "  Find text", ":Telescope live_grep <CR>"),
-        dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.lua <CR>"),
-        dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
+        dashboard.button("f", "  Find file",           ":Telescope find_files<CR>"),
+        dashboard.button("e", "  New file",            ":ene <BAR> startinsert<CR>"),
+        dashboard.button("r", "  Recently used files", ":Telescope oldfiles<CR>"),
+        dashboard.button("o", "  Open Oil",            ":Oil<CR>"),
+        dashboard.button("g", "  Find text",           ":Telescope live_grep<CR>"),
+        dashboard.button("c", "  Configuration",       ":e ~/.config/nvim/init.lua<CR>"),
+        dashboard.button("q", "  Quit Neovim",         ":qa<CR>"),
       }
-      
-      dashboard.section.footer.val = "\"seek nothing outside yourself\""
-      
+      dashboard.section.footer.val = '"seek nothing outside yourself"'
       alpha.setup(dashboard.opts)
-      vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
+      vim.cmd("autocmd FileType alpha setlocal nofoldenable")
     end,
-  }
-  
-  -- Automatically set up configuration after cloning packer.nvim
-  if packer_bootstrap then
-    require('packer').sync()
-  end
-end)
+  },
 
--- Auto-reload config on save
-vim.cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost init.lua source <afile> | PackerSync
-  augroup end
-]])
+}, {
+  ui = { border = "rounded" },
+})
 
--- Treesitter setup (protected call in case it's not installed yet)
-local status_ok, treesitter = pcall(require, 'nvim-treesitter.configs')
-if status_ok then
-  treesitter.setup({
-    ensure_installed = {
-      "lua",
-      "python",
-      "c",
-      "go",
-      "rust",
-      "bash",
-      "javascript",
-      "typescript",
-      "html",
-      "css",
-      "json",
-      "markdown",
-    },
-    sync_install = false,
-    auto_install = true,
-    highlight = {
-      enable = true,
-      additional_vim_regex_highlighting = false,
-    },
-    indent = {
-      enable = true,
-    },
-  })
+-- LSP server configuration
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+local servers = { "lua_ls", "pyright", "clangd", "rust_analyzer", "bashls", "ts_ls" }
+
+for _, server in ipairs(servers) do
+  vim.lsp.config(server, { capabilities = capabilities })
 end
-
--- Mason setup (protected call)
-local mason_ok, mason = pcall(require, 'mason')
-if mason_ok then
-  mason.setup()
-end
-
-local mason_lsp_ok, mason_lspconfig = pcall(require, 'mason-lspconfig')
-if mason_lsp_ok then
-  mason_lspconfig.setup({
-    ensure_installed = {
-      "lua_ls",
-      "pyright",
-      "clangd",
-      "rust_analyzer",
-      "bashls",
-      "ts_ls",
-    },
-    automatic_installation = true
-  })
-end
-
--- LSP Configuration (using new vim.lsp.config API for Neovim 0.11+)
-local cmp_nvim_lsp_ok, cmp_nvim_lsp = pcall(require, 'cmp_nvim_lsp')
-
-if cmp_nvim_lsp_ok then
-  local cmp_capabilities = cmp_nvim_lsp.default_capabilities()
-  
-  -- Configure LSP servers using the new API
-  local servers = { "lua_ls", "pyright", "clangd", "rust_analyzer", "bashls", "ts_ls" }
-  
-  for _, server in ipairs(servers) do
-    vim.lsp.config(server, {
-      capabilities = cmp_capabilities,
-    })
-  end
-  
-  -- Enable the configured servers
-  vim.lsp.enable(servers)
-end
-
--- Completion setup (protected call)
-local cmp_ok, cmp = pcall(require, 'cmp')
-local luasnip_ok, luasnip = pcall(require, 'luasnip')
-
-if cmp_ok and luasnip_ok then
-  cmp.setup({
-    snippet = {
-      expand = function(args)
-        luasnip.lsp_expand(args.body)
-      end,
-    },
-    window = {
-      completion = cmp.config.window.bordered(),
-      documentation = cmp.config.window.bordered(),
-    },
-    mapping = cmp.mapping.preset.insert({
-      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-      ['<C-f>'] = cmp.mapping.scroll_docs(4),
-      ['<C-Space>'] = cmp.mapping.complete(),
-      ['<C-e>'] = cmp.mapping.abort(),
-      ['<CR>'] = cmp.mapping.confirm({ select = true }),
-      ['<Tab>'] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          cmp.select_next_item()
-        elseif luasnip.expand_or_jumpable() then
-          luasnip.expand_or_jump()
-        else
-          fallback()
-        end
-      end, { 'i', 's' }),
-      ['<S-Tab>'] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          cmp.select_prev_item()
-        elseif luasnip.jumpable(-1) then
-          luasnip.jump(-1)
-        else
-          fallback()
-        end
-      end, { 'i', 's' }),
-    }),
-    sources = cmp.config.sources({
-      { name = 'nvim_lsp' },
-      { name = 'luasnip' },
-    }),
-  })
-end
-
--- Colorscheme
-vim.cmd(":hi statusline guibg=NONE")
-vim.o.background = "dark"
-vim.cmd.colorscheme "carbonfox"
-
+vim.lsp.enable(servers)
